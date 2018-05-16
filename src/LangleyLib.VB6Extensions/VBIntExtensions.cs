9f30468c8a9d33d17d150bec4bb6dea2c7269f4a
Langley <Langley228@gmail.com>
2018-05-16 15:13:47 +0800

    rename

---
+++ src/LangleyLib.VB6Extensions/VBIntExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangleyLib.VB6Extensions
+{
+    /// <summary>
+    /// 仿造 VB6 相關功能 的 Extensions
+    /// </summary>
+    public static class VBIntExtensions
+    {
+        /// <summary>
+        /// 傳回與指定字元碼關聯的字元
+        /// </summary>
+        /// <param name="Num">字元的「字碼指標」或字元碼的 Integer 運算式。</param>
+        /// <returns></returns>
+        public static char VBChr(this int Num)
+        {
+            char C = Convert.ToChar(Num);
+            return C;
+        }
+        /// <summary>
+        /// 回傳數個空白的字串
+        /// </summary>
+        /// <param name="Number">空白數量</param>
+        /// <returns></returns>
+        public static string VBSpace(this int Number)
+        {
+            if (Number > 0)
+                return "".PadRight(Number);
+            else
+                return "";
+        }
+        /// <summary>
+        /// 回傳數個0的字串
+        /// </summary>
+        /// <param name="Number">0數量</param>
+        /// <returns></returns>
+        public static string VBZero(this int Number)
+        {
+            if (Number > 0)
+                return "0".PadRight(Number, '0');
+            else
+                return "";
+        }
+    }
+}
